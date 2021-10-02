27cf5f9b139aaef7f7f1ca285c1a22bc244f45b6
Luke Edwards <luke.edwards05@gmail.com>
2021-10-01 23:36:55 -0700

    chore: setup builder

---
+++ build/index.js
@@ -0,0 +1,34 @@
+const { build } = require('esbuild');
+const pkg = require('../package.json');
+
+(async function () {
+	let shared = {
+		logLevel: 'info',
+		charset: 'utf8',
+		minify: true,
+		define: {
+			VERSION: JSON.stringify(pkg.version)
+		}
+	};
+
+	await build({
+		...shared,
+		entryPoints: ['src/bin.ts'],
+		outfile: pkg.bin,
+	});
+
+	await build({
+		...shared,
+		entryPoints: ['src/require.ts'],
+		outfile: pkg.exports['./register'].require,
+	});
+
+	await build({
+		...shared,
+		entryPoints: ['src/loader.ts'],
+		outfile: pkg.exports['./loader'].import,
+	});
+})().catch(err => {
+	console.error(err.stack || err);
+	process.exitCode = 1;
+});
