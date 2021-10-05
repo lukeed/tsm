c93553706dcd95fa9c57e5b7499e0229c0826851
Luke Edwards <luke.edwards05@gmail.com>
2021-10-05 09:05:06 -0700

    feat: Allow JavaScript extensions for TypeScript imports (#2)
    * feat(loader): map js -> ts when source is ts
    
    * feat(require): map js -> ts when source is ts
    
    * fix(require): work after minify

--- src/require.ts
+++ src/require.ts
@@ -15,6 +15,40 @@ let env = (tsm as TSM).$defaults('cjs');
 let uconf = env.file && require(env.file);
 let config: Config = (tsm as TSM).$finalize(env, uconf);
 
+declare const $$req: NodeJS.Require;
+const tsrequire = 'var $$req=require;require=(' + function () {
+	let { existsSync } = $$req('fs');
+	let { URL, pathToFileURL } = $$req('url');
+
+	return new Proxy($$req, {
+		// NOTE: only here if source is TS
+		apply(req, ctx, args: [id: string]) {
+			let [ident] = args;
+			if (!ident) return req.apply(ctx || $$req, args);
+
+			// ignore "prefix:" and non-relative identifiers
+			if (/^\w+\:?/.test(ident)) return $$req(ident);
+
+			// exit early if no extension provided
+			let match = /\.([mc])?js(?=\?|$)/.exec(ident);
+			if (match == null) return $$req(ident);
+
+			let base = pathToFileURL(__filename) as import('url').URL;
+			let file = new URL(ident, base).pathname as string;
+			if (existsSync(file)) return $$req(ident);
+
+			// ?js -> ?ts file
+			file = file.replace(
+				new RegExp(match[0] + '$'),
+				match[0].replace('js', 'ts')
+			);
+
+			// return the new "[mc]ts" file, or let error
+			return existsSync(file) ? $$req(file) : $$req(ident);
+		}
+	})
+} + ')();'
+
 function loader(Module: Module, sourcefile: string) {
 	let extn = extname(sourcefile);
 	let pitch = Module._compile!.bind(Module);
@@ -23,8 +57,13 @@ function loader(Module: Module, sourcefile: string) {
 		let options = config[extn];
 		if (options == null) return pitch(source, sourcefile);
 
+		let banner = options.banner || '';
+		if (/\.[mc]?tsx?$/.test(extn)) {
+			banner = tsrequire + banner;
+		}
+
 		esbuild = esbuild || require('esbuild');
-		let result = esbuild.transformSync(source, { ...options, sourcefile });
+		let result = esbuild.transformSync(source, { ...options, banner, sourcefile });
 		return pitch(result.code, sourcefile);
 	};
 
--- test/config/index.ts
+++ test/config/index.ts
@@ -1,10 +1,20 @@
 import * as assert from 'assert';
 
+// NOTE: doesn't actually exist yet
+import * as js from '../fixtures/math.js';
+
 // NOTE: avoid need for syntheticDefault + analysis
 import * as data from '../fixtures/data.json';
 assert.equal(typeof data, 'object');
 
 // @ts-ignore - generally doesn't exist
 assert.equal(typeof data.default, 'string');
+
+// NOTE: raw JS missing
+assert.equal(typeof js, 'object', 'JS :: typeof');
+assert.equal(typeof js.sum, 'function', 'JS :: typeof :: sum');
+assert.equal(typeof js.div, 'function', 'JS :: typeof :: div');
+assert.equal(typeof js.mul, 'function', 'JS :: typeof :: mul');
+assert.equal(js.foobar, 3, 'JS :: value :: foobar');
 
 console.log('DONE~!');
