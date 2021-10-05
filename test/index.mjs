ce3973c2c5b77ea8239fc1b141b389dafea2ceeb
Luke Edwards <luke.edwards05@gmail.com>
2021-10-05 09:03:54 -0700

    chore: add "mts" and "cts" fixtures;
    - typescript file uses "mjs" and "cjs" imports
    - javascript files use real "mts" and "cts" imports

--- test/index.mjs
+++ test/index.mjs
@@ -3,8 +3,14 @@ import assert from 'assert';
 
 import jsx from './fixtures/App1';
 import json from './fixtures/data.json';
-import * as ts from './fixtures/math';
-import tsx from './fixtures/App2';
+// @ts-ignore – expects definitions
+import * as mts from './fixtures/utils.mts';
+// @ts-ignore – expects definitions
+import * as cts from './fixtures/utils.cts';
+// @ts-ignore – prefers extensionless
+import * as ts from './fixtures/math.ts';
+// @ts-ignore – prefers extensionless
+import tsx from './fixtures/App2.tsx';
 
 const props = {
 	foo: 'bar'
@@ -38,5 +44,13 @@ assert.equal(typeof ts.sum, 'function', 'TS :: typeof :: sum');
 assert.equal(typeof ts.div, 'function', 'TS :: typeof :: div');
 assert.equal(typeof ts.mul, 'function', 'TS :: typeof :: mul');
 assert.equal(ts.foobar, 3, 'TS :: value :: foobar');
+
+assert.equal(typeof mts, 'object', 'MTS :: typeof');
+assert.equal(typeof mts.capitalize, 'function', 'MTS :: typeof :: capitalize');
+assert.equal(mts.capitalize('hello'), 'Hello', 'MTS :: value :: capitalize');
+
+assert.equal(typeof cts, 'object', 'CTS :: typeof');
+assert.equal(typeof cts.dashify, 'function', 'CTS :: typeof :: dashify');
+assert.equal(cts.dashify('FooBar'), 'foo-bar', 'CTS :: value :: dashify');
 
 console.log('DONE~!');
