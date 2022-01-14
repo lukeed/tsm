25fba3398051ba07a2516122b2e2c03103963e07
C. Lewis <ctj.lewis@icloud.com>
2022-01-14 06:38:57 -0600

    chore: proof of concept

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -2,19 +2,29 @@ import { build as esbuild, BuildOptions } from "esbuild";
 import { readFile, rm } from "fs/promises";
 import chalk from "chalk";
 import glob from "fast-glob";
-import { resolve as resolvePath } from "path";
+import { dirname, extname, isAbsolute, relative, resolve as resolvePath } from "path";
 
 import { Plugin as RollupPlugin, rollup } from "rollup";
 import { RenderedChunk } from "rollup";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore No types available for this plugin.
 import shebang from "rollup-plugin-preserve-shebang";
-import { pathToFileURL } from "url";
+import { fileURLToPath, pathToFileURL } from "url";
+import { resolve } from "../loader/index.js";
 
 /**
  * This import breaks everything.
  */
 // import { resolve } from "../loader/index.js";
+
+const getRelativePath = (baseUrl: string, path: string) => {
+  const relativePath = relative(baseUrl, path);
+  return (
+    relativePath.startsWith(".")
+      ? relativePath
+      : `./${relativePath}`
+  );
+};
 
 /**
  * Matches a complete import statement, including the import keyword, as well as
@@ -39,6 +49,7 @@ export const rewriteImport = (
   importToReplace: string,
   importReplacement: string,
 ) => {
+  console.log({ importStatement, importToReplace, importReplacement });
   const [_, sourcePart] = importStatement.split(/from|\(/);
   const rewrittenSource = sourcePart
     .replace(importToReplace, importReplacement)
@@ -50,47 +61,99 @@ export const rewriteImport = (
 export const rewriteImports: () => RollupPlugin = () => {
   return {
     name: "Rewrite imports",
-    // renderChunk: async (code: string, chunk, options) => {
-    //   for (const chunkImport of chunk.imports) {
-    //     // console.log({ chunkImport, code });
-    //     /**
-    //      * Read the matched import/require statements and replace them.
-    //      */
-    //     const importMatch = importPattern(chunkImport);
-    //     const importStatements = code.match(importMatch) ?? [];
-    //     for (const importStatement of importStatements) {
-    //       if (options.file) {
-    //         const parentURL = pathToFileURL(resolvePath(options.file)).href;
-    //         const resolvedImport = await resolve(
-    //           chunkImport,
-    //           {
-    //             parentURL,
-    //             conditions: [ "node", "import", "node-addons" ]
-    //           },
-    //           async (url) => await import(url),
-    //         );
-    //         /**
-    //           * Rewrite import identifiers for seamless CJS support. Ignore dynamic
-    //           * imports.
-    //           */
-    //         // const rewrittenImport = rewriteImport(
-    //         //   importStatement,
-    //         //   chunkImport,
-    //         //   resolvedImport.url,
-    //         // );
-    //         // console.log(parentURL);
-    //         console.log(chunkImport);
-    //         console.log(resolvedImport.url);
-    //         // code = code.replace(importStatement, rewrittenImport);
-    //       }
-    //     }
-    //   }
+    renderChunk: async (code: string, chunk, options) => {
+      for (const importedChunk of chunk.imports) {
+        // console.log({ importedChunk });
+        /**
+         * If no absolute module ID, bail.
+         */
+        const input = chunk.facadeModuleId;
+        if (!input) continue;
+
+        /**
+         * Just a named module. Skip.
+         */
+        if (!importedChunk.includes("/")) {
+          console.log(`Skipping named module: ${importedChunk}`);
+          continue;
+        }
+
+        if (extname(importedChunk)) {
+          /**
+           * Full import specifiers (are absolute and have file extensions) are
+           * never rewritten.
+           */
+          if (isAbsolute(importedChunk)) {
+            console.log(`Ignoring absolute specifier: ${importedChunk}`);
+            continue;
+          }
+          /**
+           * Otherwise, just a package ending in `.js` or something, or some
+           * other incomplete specifier that must be resolved.
+           */
+        }
+
+        const baseDir = dirname(input);
+        let importToReplace = importedChunk;
+
+        /**
+         * Rewrite remaining absolute specifiers relative to baseDir for
+         * replacement.
+         */
+        if (isAbsolute(importedChunk)) {
+          console.log(`Rewriting partial specifier: ${importedChunk}`);
+          importToReplace = getRelativePath(baseDir, importedChunk);
+        }
+
+        /**
+         * Read the matched import/require statements and replace them.
+         */
+        const importMatch = importPattern(importToReplace);
+        const importStatements = code.match(importMatch) ?? [];
+        // console.log({ importStatements });
+
+        // console.log({ file: options.file, imports: chunk.imports, importStatements });
+        for (const importStatement of importStatements) {
+          if (options.file) {
+            const parentURL = pathToFileURL(resolvePath(input)).href;
+            const resolvedImport = await resolve(
+              importedChunk,
+              {
+                parentURL,
+                conditions: [ "node", "import", "node-addons" ]
+              },
+              async (url) => await import(url),
+            );
+            /**
+             * Rewrite import identifiers for seamless CJS support. Ignore dynamic
+             * imports.
+             */
+            if (resolvedImport.url) {
+              const unixLikePath = resolvedImport.url.replace("file://", "");
+              const rewrittenImport = rewriteImport(
+                importStatement,
+                importToReplace,
+                getRelativePath(baseDir, unixLikePath),
+              );
+
+              console.log({ input, importStatement, rewrittenImport });
+              code = code.replace(importStatement, rewrittenImport);
+            }
+            // console.log(parentURL);
+            // console.log(rewrittenImport);
+          }
+        }
+      }
       
-    //   return null;
-    //   // for (const chunkImport of chunk.imports) {
-    //   //   console.log({ chunkImport });
-    //   // }
-    // },
+      // return null;
+      return {
+        code,
+        sourcemap: false,
+      };
+      // for (const chunkImport of chunk.imports) {
+      //   console.log({ chunkImport });
+      // }
+    },
   };
 };
 
@@ -134,6 +197,7 @@ export const postBuild = async () => {
   await Promise.all(
     filesToOptimize.map(
       async (file) => {
+        console.log("Optimizing", file);
         const build = await rollup({
           input: file,
           external: (id: string) => id !== file,
@@ -141,7 +205,7 @@ export const postBuild = async () => {
           // onwarn: () => void 0,
         });
 
-        await build.generate({
+        await build.write({
           file,
           format: "es",
         });
